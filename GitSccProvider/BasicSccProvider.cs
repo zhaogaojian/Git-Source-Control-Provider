aa8f468f0cafd15004531b31eb77aaa111983095
Jon Zoss <jon.zoss@target.com>
2015-11-25 13:22:58 -0600

    Fix for issue #5

--- GitSccProvider/BasicSccProvider.cs
+++ GitSccProvider/BasicSccProvider.cs
@@ -488,9 +488,11 @@ namespace GitScc
 
         private void ShowHistoryWindow(object sender, EventArgs e)
         {
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            path = Path.Combine(path, "Dragon.pkg");
+            var workingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var path = Path.Combine(workingPath, "Dragon.pkg");
             var tmpPath = Path.Combine(Path.GetTempPath(), "Dragon.exe");
+            var gitPath = Path.Combine(workingPath, "NativeBinaries"); 
 
             var needCopy = !File.Exists(tmpPath);
             if(!needCopy)
@@ -505,6 +507,7 @@ namespace GitScc
                 try
                 {
                     File.Copy(path, tmpPath, true);
+                    DirectoryCopy(gitPath, Path.GetTempPath() + "NativeBinaries",true);
                 }
                 catch // try copy file silently
                 {
@@ -513,7 +516,45 @@ namespace GitScc
 
             if (File.Exists(tmpPath) && sccService.CurrentTracker != null)
             {
-                Process.Start(tmpPath, "\"" + sccService.CurrentTracker.WorkingDirectory + "\"");
+                Process.Start(tmpPath, sccService.CurrentTracker.WorkingDirectory);
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
             }
         }
 
