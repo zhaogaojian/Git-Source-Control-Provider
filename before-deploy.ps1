4271d474b39882f725034721109570b049df7de5
Jon Zoss <jon.zoss@target.com>
2016-03-01 10:50:05 -0600

    Build updated

---
+++ before-deploy.ps1
@@ -0,0 +1,54 @@
+﻿$gitHubUsername = "$env.gitHubUsername" 
+$gitHubRepository = "$env.gitRepo"
+$tagName = "GSCP-Dev-v$env:majorVersion.$env.minorVersion"
+$gitHubApiKey = "$env.access_token"
+
+ $getReleaseParams = @{
+   Uri = "https://api.github.com/repos/$gitHubUsername/$gitHubRepository/releases/tags/$tagName";
+   Method = 'GET';
+   Headers = @{
+     Authorization = 'token ' + $gitHubApiKey
+   }
+ }
+
+$relId;
+
+Try
+{
+	$release = Invoke-RestMethod @getReleaseParams 
+	$relId = $release.id
+}
+Catch
+{
+	Write-Host "Release not found" 
+  Exit;
+}
+
+if (!$relId)
+{
+	Write-Host "Release not found" 
+	Exit;
+}
+
+
+
+ $deleteReleaseParams = @{
+   Uri = "https://api.github.com/repos/$gitHubUsername/$gitHubRepository/releases/$relId";
+   Method = 'DELETE';
+   Headers = @{
+      Authorization = 'token ' + $gitHubApiKey
+   }
+ }
+
+  $deleteTagParams = @{
+   Uri = "https://api.github.com/repos/$gitHubUsername/$gitHubRepository/git/refs/tags/$tagName";
+   Method = 'DELETE';
+   Headers = @{
+     Authorization = 'token ' + $gitHubApiKey
+   }
+      ContentType = 'application/json';
+   Body = ""
+ }
+
+  $result = Invoke-RestMethod @deleteReleaseParams 
+  $result = Invoke-RestMethod @deleteTagParams 
