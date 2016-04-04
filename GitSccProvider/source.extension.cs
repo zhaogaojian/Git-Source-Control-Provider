365c60339c4d38dcfea21586f41db544ce83db3d
Jon Zoss <jon.zoss@target.com>
2016-04-04 13:25:24 -0500

    Test for new autoversion

---
+++ GitSccProvider/source.extension.cs
@@ -0,0 +1,13 @@
+namespace GitSccProvider
+{
+    static class Vsix
+    {
+        public const string Id = "GitSccProvider.Microsoft.88d658b3-e361-4e7f-8f4d-9e78f6e4515a";
+        public const string Name = "Git Source Control Provider 2015";
+        public const string Description = "Git Source Control Provider is a plug-in that integrates git with Visual Studio.";
+        public const string Language = "en-US";
+        public const string Version = "1.5.0.0";
+        public const string Author = "Jon Zoss";
+        public const string Tags = "";
+    }
+}
